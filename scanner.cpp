84a77f1d7ad26195359e53b5eaaf109fa28c17db
nandogebru <119262784+nandogebru@users.noreply.github.com>
2024-02-16 15:24:27 -0600

    having the array in the token.h and trying to access it in different files is creating multiple decleration. lets try declaring as extern and intializing it only in testScanner.cpp

--- scanner.cpp
+++ scanner.cpp
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "scanner.h"
+
+
 // we need a map function here that is going to read in the char, and it's going to be based on what that char is,
 // its gonna map it to a number that i decide. > = 0 
 
--- token.h
+++ token.h
@@ -19,7 +19,7 @@ typedef enum{
     T3_tk  // . ! , ,; ? ?$ * 8'
 }tokenID;
 //string tokenNames[] = {"T1 token", "T1 token", "T1 token"};
-const char* tokenNames[] = {"EOF token","T1 token","T2 token", "T2 token"};
+extern const char* tokenNames[];// = {"EOF token","T1 token","T2 token", "T2 token"};
 
 // for each token we are going to need the TokenID, TokenInstance, line#
 struct Token{
