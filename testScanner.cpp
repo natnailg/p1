74d45baf9c49fe0835207d089947feaa6abb5113
nandogebru <119262784+nandogebru@users.noreply.github.com>
2024-02-16 14:31:30 -0600

    trying to get the token type to print from the char array

--- testScanner.cpp
+++ testScanner.cpp
@@ -7,5 +7,5 @@
 //#include "token.h"
 //
 void hellotestscanner(struct Token mytokens, const char* tokenNames[]){
-    printf("Token EOF: %d\n",mytokens.tokenId, tokenNames[mytokens.tokenId]);
+    printf("Token EOF: %s-----%s\n",mytokens.tokenId, tokenNames[mytokens.tokenId]);
 }
