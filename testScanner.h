6f48f28cbe6a99e62e8570578732b0ec7a0c3ec1
nandogebru <119262784+nandogebru@users.noreply.github.com>
2024-02-23 14:43:10 -0600

    changing define to const int

--- testScanner.h
+++ testScanner.h
@@ -7,18 +7,30 @@
 
 void printtable();
 //states
-#define LETTER          0
-#define DIGIT           1
-#define PERCENTAGE      2
-#define DOT_OR_EXCLAMATION  3
-#define COMMA           4
-#define SEMICOLON       5
-#define QUESTION_MARK   6
-#define DOLLAR_SIGN     7
-#define STAR            8
-#define QUOTATION       9
-#define WHITESPACE      10
-#define END_OF_FILE     11
+const int LETTER = 0;
+const int PERCENTAGE = 1;
+const int DOT_OR_EXCLAMATION  = 3;
+const int COMMA = 4;
+const int SEMICOLON = 5;
+const int QUESTION_MARK = 6;
+const int DOLLAR_SIGN = 7;
+const int STAR = 8;
+const int QUOTATION = 9;
+const int WHITESPACE = 10;
+const int END_OF_FILE = 11;
+
+//#define LETTER          0
+//#define DIGIT           1
+//#define PERCENTAGE      2
+//#define DOT_OR_EXCLAMATION  3
+//#define COMMA           4
+//#define SEMICOLON       5
+//#define QUESTION_MARK   6
+//#define DOLLAR_SIGN     7
+//#define STAR            8
+//#define QUOTATION       9
+//#define WHITESPACE      10
+//#define END_OF_FILE     11
 
 void scanners();
 int mapingchar(char );
