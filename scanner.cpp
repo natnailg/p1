4603697735efdd2f4bccf1e5c17baa6d5db7f980
nandogebru <119262784+nandogebru@users.noreply.github.com>
2024-02-25 16:13:44 -0600

    hoping to God this is able to read and find token for all the strings of characters.!!

--- scanner.cpp
+++ scanner.cpp
@@ -6,9 +6,6 @@
 
 
 const char* tokenNames[] = {"Unknown token", "EOF token", "T1 token", "T2 token", "T3 token"};
-void token_description(struct Token tokens){
-    printf("Token description: %s\n", tokenNames[tokens.tokenId]);
-}
 
 //FSA Table have row and column ordering as specified in class
 int Table [12][12]= { //had to do 12 for the columns
@@ -61,31 +58,92 @@ int mapingchar(char c) {
     }
 }
 
-tokenID token(int num, char* fullString) {
-    printf("token function called --> %c\n", fullString);
-    switch (num) {
-        case 1001:
-            printf("is ever char coming here !!= %c", fullString);
-            printf("Token: %s - Full String: %s\n", tokenNames[1], fullString);
-            break;
-        case 1002:
-            printf("Token: %s - Full String: %s\n", tokenNames[2], fullString);
-            break;
-        case 1003:
-            printf("Token: %s - Full String: %s\n", tokenNames[3], fullString);
-            break;
-        case 1004:
-            printf("i am in token 3\n");
-            printf("Token: %s - Full String: %s\n", tokenNames[4], fullString);
-            break;
-        case -1:
-            printf("Token: %s - Full String: %s\n", tokenNames[0], fullString);
-            return Error;
-        default:
-            printf("Token: %s - Full String: %s\n", tokenNames[0], fullString);
-            return unknown;
+
+tokenID FADriver(char* tokeninstances) {
+    int state = 0;
+    int nextState;
+    char S[256] = ""; // Assuming maximum token length of 255 characters
+    int index = 0; // Index for tokeninstances
+    int S_index = 0; // Index for S array
+
+    char nextChar = tokeninstances[index++]; // Initialize nextChar with the first character in tokeninstances
+
+    while (1) { // Loop until the end of the string ('\0') is reached
+        int column = mapingchar(nextChar); // Get column index using mappingchar function
+        nextState = Table[state][column];
+
+        if (nextState < 0) {
+            switch (nextState) {
+                case -1:
+                    printf("can't start with a digit! %s\n", S);
+                    return Error ;
+                case -2:
+                    printf("semicolone Error!!\n %s", S);
+                    return Error;
+                case -3:
+                    printf("can't have dollar sign start %s \n", S);
+                    return Error;
+                case -4 :
+                    printf("CAN'T START WITH STAR!! %s\n", S);
+                    return Error;
+                case -5 :
+                    printf("must be followed by a digit %s\n", S);
+                    return Error;
+                case -6 :
+                    printf("must be followed by a digit %s\n", S);
+                    return Error;
+                default:
+                    printf("unknown Error %s\n",S);
+                    return unknown;
+            }
+        }
+
+        if (nextState > 1000) {
+            // Final state reached, return the token
+            S[S_index] = '\0'; // Null-terminate the string
+            switch (nextState) {
+                case 1001:
+                    printf("%s \n\n", tokenNames[1]);
+                    return EOFtk;
+                case 1002:
+                    printf("Token: %s - Full String: %s\n\n", tokenNames[2], S);
+                    break;
+                case 1003:
+                    printf("Token: %s - Full String: %s\n\n", tokenNames[3], S);
+                    break;
+                case 1004:
+                    printf("Token: %s - Full String: %s\n\n", tokenNames[4], S);
+                    break;
+                default:
+                    printf("Token: %s - Full String: %s\n\n", tokenNames[0], S);
+                    return unknown;
+            }
+            state = 0; // Reset the state to zero
+            S_index = 0; // Reset the index for S array
+
+            //reset the array
+            for (int i = 0; i < 256; i++) {
+                S[i] = '\0';
+            }
+            S[0] = nextChar; // Append the character to the string
+            // nextChar = tokeninstances[index++]; // Read the next character from tokeninstances
+
+            column = mapingchar(nextChar); // Get column index using mappingchar function
+            nextState = Table[state][column];
+            // printf("%sinside the switch------------------next state %d----------------%c, column %d\n", S, nextState, nextChar, column);
+
+
+        } else {
+            // Not in final state yet
+            state = nextState;
+            S[S_index++] = nextChar; // Append the character to the string
+            nextChar = tokeninstances[index++]; // Read the next character from tokeninstances
+        }
     }
 }
+
+
+
 
 //
 //tokenID FADriver(char* tokeninstances) {
@@ -129,40 +187,29 @@ tokenID token(int num, char* fullString) {
 //
 
 
-tokenID FADriver(char* tokeninstances) {
-//    printf("Received token string in FADriver: %s\n", tokeninstances);
-    int state = 0;
-    int nextState;
-    char S[256] = ""; // Assuming maximum token length of 255 characters
-    int index = 0;
-    nextChar = tokeninstances[index++]; // Initialize nextChar with the first character in tokeninstances
-
-    while (1) { // Loop until the end of the string ('\0') is reached
-        int column = mapingchar(nextChar); // Get column index using mappingchar function
-        printf("after mapping column : %c mapingchar : %c \n", column, nextChar);
-        if (column == -1) {
-            printf("Error: Unknown character.\n");
-            return Error; // Return an error token
-        }
-
-        nextState = Table[state][column];
-        if (nextState < 0) {
-            printf("Error: Negative number from table, something is invalid.\n");
-            return Error; // Return an error token
-        }
-        if (nextState > 1000) {
-//            printf("large int found!!\n");
-            // Final state reached, return the token
-            S[index - 1] = '\0'; // Null-terminate the string
-
-//            printf("before the nextstate: %d ---> %d ---char->%c ---> %s\n", state, column, nextChar, S);
-            return token(nextState, S); // Return the token obtained from the token function along with the full string
-        } else {
-            // Not in final state yet
-            state = nextState;
-            S[index - 1] = nextChar; // Append the character to the string
-            nextChar = tokeninstances[index++]; // Read the next character from tokeninstances
-        }
-    }
-
-}
+//tokenID token(int num, char* fullString) {
+//    printf("token function called --> %c\n", fullString);
+//    switch (num) {
+//        case 1001:
+//            printf("is ever char coming here !!= %c", fullString);
+//            printf("Token: %s - Full String: %s\n", tokenNames[1], fullString);
+//            break;
+//        case 1002:
+//            printf("Token: %s - Full String: %s\n", tokenNames[2], fullString);
+//            break;
+//        case 1003:
+//            printf("Token: %s - Full String: %s\n", tokenNames[3], fullString);
+//            break;
+//        case 1004:
+//            printf("i am in token 3\n");
+//            printf("Token: %s - Full String: %s\n", tokenNames[4], fullString);
+//            break;
+//        case -1:
+//            printf("Token: %s - Full String: %s\n", tokenNames[0], fullString);
+//            return Error;
+//        default:
+//            printf("Token: %s - Full String: %s\n", tokenNames[0], fullString);
+//            return unknown;
+//    }
+//    return unknown;
+//}
