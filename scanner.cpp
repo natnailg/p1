5d6dcfca512622a2fdde68e0614441a182b91d3f
nandogebru <119262784+nandogebru@users.noreply.github.com>
2024-02-25 21:30:19 -0600

    it might be the default -1 when mapping.

--- scanner.cpp
+++ scanner.cpp
@@ -42,7 +42,6 @@ int mapingchar(char c) {
         case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
         case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
         case 'V': case 'W': case 'X': case 'Y': case 'Z':
-            printf("digit ->> %c ", c);
             return LETTER;
         case '0': case '1': case '2': case '3': case '4':
         case '5': case '6': case '7': case '8': case '9':
@@ -72,7 +71,7 @@ int mapingchar(char c) {
             return END_OF_FILE; // END_OF_FILE
         default:
 //            printf("mapping %c --> \n",c );
-            return 0;// Unknown character
+            return -1;// Unknown character
     }
 }
 
