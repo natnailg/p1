d3f96935dfc5550a25e4da28be6a5443efe38240
nandogebru <119262784+nandogebru@users.noreply.github.com>
2024-02-25 18:47:47 -0600

    wokring on EOFtoken

--- scanner.cpp
+++ scanner.cpp
@@ -70,7 +70,7 @@ tokenID FADriver(char* tokeninstances, int line_num) {
 
     nextChar = tokeninstances[index++]; // Initialize nextChar with the first character in tokeninstances
 //
-    while (1) { // Loop until the end of the string ('\0') is reached
+    while (nextChar != EOF) { // Loop until the end of the string ('\0') is reached
         int column = mapingchar(nextChar); // Get column index using mappingchar function
         nextState = Table[state][column];
 
