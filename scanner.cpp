330ff1ff61d538b88bf6c2b0d38522ed50d41770
nandogebru <119262784+nandogebru@users.noreply.github.com>
2024-02-25 21:04:43 -0600

    it might be the default -1 when mapping.

--- scanner.cpp
+++ scanner.cpp
@@ -76,7 +76,7 @@ int mapingchar(char c) {
 //            return END_OF_FILE;
         default:
             printf("mapping %c --> \n",c );
-            return -1;// Unknown character
+            return 0;// Unknown character
     }
 }
 
