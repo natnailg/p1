6edf07397a249f0eb4fa1828564ea24412956e0f
nandogebru <119262784+nandogebru@users.noreply.github.com>
2024-03-04 09:56:41 -0600

    back to problem with line number.

--- testScanner.cpp
+++ testScanner.cpp
@@ -31,18 +31,11 @@ void readFromFile(char* filename) {
         if (input_char == '\n') {
             // Increment line number
             token.line_num++;
-            // Process the token
-            FADriver(token.tokeninstance, token.line_num);
-
-            // Reset tokeninstance and index for the next token
-            memset(token.tokeninstance, 0, MAX_INSTANCE_TOKEN);
-            index = 0;
             continue;
         } else if (input_char == EOF) {
             // End of file reached, store the last token instance and break the loop
             token.tokeninstance[index] = '\0'; // Null-terminate the token instance
             // printf("final %s  %d\n", token.tokeninstance, token.line_num);
-            FADriver(token.tokeninstance, token.line_num);
             break;
 
         } else {
@@ -57,8 +50,9 @@ void readFromFile(char* filename) {
                 break;
             }
         }
+        FADriver(token.tokeninstance, token.line_num);
     } while (1);
-    //FADriver(token.tokeninstance, token.line_num);
+
 
 
     fclose(file);
